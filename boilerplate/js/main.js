19fc1581f76631f74965dac2e2b525105fce3b13
JXS <jimxshaw@gmail.com>
2015-11-20 23:03:05 -0500

    backbone model validation

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -2,3 +2,13 @@
 // In the first few sections, we do all the coding here.
 // Later, you'll see how to organize your code into separate
 // files and modules.
+
+var Car = Backbone.Model.extend({
+  validate: function(attrs) {
+    if (!attrs.make) {
+      return "the car's make is required";
+    }
+  }
+});
+
+var car = new Car();
